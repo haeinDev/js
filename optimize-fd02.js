0eb70c1a83a40ca984addd4c660b01a7f644d6bc
haeinjane <49984707+haeinjane@users.noreply.github.com>
2020-11-25 10:17:02 +0000

    Update optimize-fd02.js

--- optimize-fd02.js
+++ optimize-fd02.js
@@ -1,6 +1,3 @@
-  var secondFieldSet = document.querySelector(
-          'form#lf_requestInformationForm>fieldset:nth-of-type(2)>div:nth-of-type(6)>div>label'
-        );
         
    $("#lf_requestInformationForm>fieldset:nth-of-type(2)>div:nth-of-type(6)>div>label" ).append('<div class="tooltip"><i class="fa fa-info-circle inforIcon"></i><p>what is this?</p><span class="tooltiptext">Available cash:<br> Available cash includes cash deposits, savings, cash from family and friends and liquid assets (e.g. property, stocks, bonds) which can be easily converted to cash.</span></div>');
           secondFieldSet.setAttribute("style", "display:flex;");
